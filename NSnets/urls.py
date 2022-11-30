85eac23c688e1dee1d8b43e7b035280c35081e03
Codeburst09 <meher.madhavareddy@gmail.com>
2022-11-30 11:24:41 +0530

    first

--- NSnets/urls.py
+++ NSnets/urls.py
@@ -3,22 +3,23 @@ from . import views
 
 urlpatterns = [path('',views.index),
 path('About/',views.About),
-path('Balcony/',views.Balcony),
+path('balcony-safety-net/',views.Balcony),
 path('BirdPro/',views.BirdProtect),
 path('Birdspike/',views.BirdSpikes),
 path('cocosafe/',views.ConsafafteyNets),
-path('Child/',views.ChildernSafe),
+path('children-safety-net/',views.ChildernSafe),
 path('Carparkk/',views.Carpark),
-path('Coconut/',views.Coconut),
+path('coconut-safety-net/',views.Coconut),
 path('Cricket/',views.Cricket),
-path('Duct/',views.DuctSafe),
-path('GlassSafee/',views.GlassSafe),
+path('duct-area-safety-net/',views.DuctSafe),
+path('glass-safety-net/',views.GlassSafe),
 path('industrys/',views.industrysafe),
-path('Monkey/',views.Monkeysafe),
-path('PigeonS/',views.PigeonSafe),
+path('/monkey-safety-net/',views.Monkeysafe),
+path('pigeon-safety-net/',views.PigeonSafe),
 path('PigeonNet/',views.PigeonNet),
 path('StairSafe/',views.StaircaseSfatey),
 path('SwimSSafe/',views.SwimmingSafe),
+#path('sports-net/')
 
 
 
